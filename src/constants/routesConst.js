c3032f3527eb1e3613c2b4045fd1d52914890707
Carlos Perez <solracias@gmail.com>
2018-05-06 12:26:14 -0700

    set up routesConst.js file with HOME route

---
+++ src/constants/routesConst.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// import API's
+// do this later
+
+// define main pages
+const mainPages = {
+  HOME: '/'  
+}
+
+
+
+// export LINKS
+// may need several
+// after clicking new
+// after clicking save
+// after clicking edit
+// after clicking update
+// after clicking delete
+// or
+/* 
+i can update the array onChange,
+componentDidMount, ComponenetDidUpdate,
+etc.
+*/
+
+// export routes to main pages
+export const ROUTES = {
+  HOME: mainPages.HOME
+}
