53e79388fa2833de7382a5cb27dfcb28e04ca082
Carlos Perez <solracias@gmail.com>
2018-05-13 21:47:15 -0700

    craeted blank form view and deleted some comments

---
+++ src/components/FormView.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// may need to import a LINKS const
+// here
+
+class FormView extends React.Component {
+  render() {
+    return {
+      
+    }
+  }
+}
--- src/routedViews/routes.js
+++ src/routedViews/routes.js
@@ -3,11 +3,8 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-// still need to define this component
 import PasswordListView from '../components/PasswordListView';
 
-
-//import routes const
 import {
   ROUTES
 } from '../constants/routesConst';
